096b24fb16244a9a45ec0e4c8f1fcee10d7f979d
Yinon Ozery <yinonozery@gmail.com>
2022-03-28 19:58:50 +0300

    Updates

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Form from './Form'
 import Login from './Login';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Register from './Register';
+import Reset from './Reset';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
             <Route path="/" element={<Login />} />
             <Route path="/dashboard" element={<Form />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/reset" element={<Reset />} />
           </Routes>
       </Router>
     </div>
---
+++ src/Reset.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { sendPasswordReset } from "./firebase";
+
+import "./Login.css";
+
+function Reset() {
+    const [email, setEmail] = useState("");
+
+    const sendPassword = () => {
+        sendPasswordReset(email)
+            .catch(function (error) {
+                alert(error)
+            });
+    }
+
+    return (
+        <div className="login">
+            <div className="login_container">
+                <img src="https://static.wixstatic.com/media/231ad7_7b2caab76cf4460b81167f13d65a5302~mv2.jpg/v1/fit/w_2500,h_1330,al_c/231ad7_7b2caab76cf4460b81167f13d65a5302~mv2.jpg" height='300' width='300' />
+                <h1>שכחתי סיסמה</h1>
+                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="כתובת אימייל" required />
+                <br />
+                <button className="btn-login" onClick={sendPassword}>שלח</button>
+            </div>
+        </div>
+    );
+}
+export default Reset;
