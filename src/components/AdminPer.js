e15731377bb0fad0251a8d265c05f66221c0541c
Yinon Ozery <yinonozery@gmail.com>
2022-05-11 11:04:14 +0300

    Update

--- src/components/AdminPer.js
+++ src/components/AdminPer.js
@@ -1,29 +1,35 @@
-import { db, auth } from "./Firebase";
-import { getAuth } from "firebase/auth";
+ import { getAuth } from "firebase/auth";
 import { collection, getDocs } from "firebase/firestore";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useAuthState } from "react-firebase-hooks/auth";
+import { onAuthStateChanged } from "firebase/auth";
 
 const AdminPer = () => {
     const navigate = useNavigate();
-    const user = useAuthState(auth);
+    const user = getAuth();
     let isAdmin = false;
 
     useEffect(() => {
-        getDocs(collection(db, "users")).then((data) => {
-            data.forEach((doc) => {
-                if (getAuth().currentUser && getAuth().currentUser.uid === doc.data().uid && doc.data().isAdmin) {
-                    console.log('Logged in as administrator (', doc.data().name, ')');
-                    isAdmin = true;
-                    navigate('/admin');
-                };
-            });
-        }).then(() => {
-            if (getAuth().currentUser && isAdmin === false)
-                navigate('/dashboard');
-        });;
+        onAuthStateChanged(user, (user) => {
+            if (user) {
+                getDocs(collection(db, "users")).then((data) => {
+                    data.forEach((doc) => {
+                        if (!isAdmin && user && user.uid === doc.data().uid && doc.data().isAdmin) {
+                            console.log('Logged in as administrator (', doc.data().name, ')');
+                            isAdmin = true;
+                            navigate('/admin');
+                        }
+                    });
+                    if (user && isAdmin === false)
+                        navigate('/dashboard');
+                });
+            } else {
+                console.log("User not connected");
+                navigate('/login');
+            }
+        });
     }, [user]);
+
     return null;
 };
 
